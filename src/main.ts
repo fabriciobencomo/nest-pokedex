0415309300b48f954b87898dae5e94ca3792f067
Fabricio <fabriciobencomo.viloria@gmail.com>
2025-04-30 03:20:34 -0400

    pagination and limit

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,11 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions:{
+        enableImplicitConversion: true
+
+      }
     })  
   )
 
